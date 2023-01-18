7ddbb4946a088318ea1e81db2d6aa0bac82cd0bd
Zoe Rorvig <zrorvig@gmail.com>
2023-01-17 21:05:46 -0500

    get all tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,9 +3,18 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+    const tagData = await Tag.findAll({
+      include: [{
+        model: Product,
+      }]
+    });
+    res.json(tagData);
+  } catch (err) {
+    console.error(err);
+    res.json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
